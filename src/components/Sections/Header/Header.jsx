50f1a8ffb9b3cb1a523bce8b2ffb793615b46008
kayloportal <pinobaxp@gmail.com>
2024-02-22 19:14:25 +0330

    updated/ added container into the Header jsx

--- src/components/Sections/Header/Header.jsx
+++ src/components/Sections/Header/Header.jsx
@@ -1,12 +1,14 @@
-import "./Header.css"
+import "./Header.css";
 
 function Header() {
   return (
-    <header className="header">
-      <img className="header__logo" src="/logo.svg" alt="brix templates" />
-      <button className="header__button"></button>
-    </header>
-  )
+    <div className="container">
+      <header className="header">
+        <img className="header__logo" src="/logo.svg" alt="brix templates" />
+        <button className="header__button"></button>
+      </header>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
