984461304403e48921d9c1f191b11ee2fdabbfd3
kayloportal <pinobaxp@gmail.com>
2024-02-22 19:31:58 +0330

    updated/ Header's JSX

--- src/components/Sections/Header/Header.jsx
+++ src/components/Sections/Header/Header.jsx
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header className="header">
       <div className="header-container | container">
-        <img className="header__logo" src="/logo.svg" alt="brix templates" />
-        <button className="header__button | clr-neutral-100 fs-other-500">Clone now</button>
+        <img className="header__logo" src="/logo.svg" alt="Brix templates" />
+        <button className="header__button | clr-neutral-100 fs-other-500 fw-bold">Clone now</button>
       </div>
     </header>
   );
