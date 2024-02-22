d6738c932811101360409b792149bb38736b5865
kayloportal <pinobaxp@gmail.com>
2024-02-22 20:09:58 +0330

    bugfix/ semantic fix for header

--- src/components/Sections/Header/Header.jsx
+++ src/components/Sections/Header/Header.jsx
@@ -2,12 +2,12 @@ import "./Header.css";
 
 function Header() {
   return (
-    <header className="header">
+    <div className="header">
       <div className="header-container | container">
         <img className="header__logo" src="/logo.svg" alt="Brix templates" />
         <button className="header__button | clr-neutral-100 fs-other-500 fw-bold">Clone now</button>
       </div>
-    </header>
+    </div>
   );
 }
 
