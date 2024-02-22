632138a35d464616aa85f6e301ea0c083499e470
kayloportal <pinobaxp@gmail.com>
2024-02-22 20:28:04 +0330

    added/ Hero's Base JSX & check-icon

--- src/components/Sections/Hero/Hero.jsx
+++ src/components/Sections/Hero/Hero.jsx
@@ -1,9 +1,29 @@
-import "./Hero.css"
+import "./Hero.css";
+import iconCheck from "/public/icon-check.svg" 
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="hero">
+      <div className="hero-container | container">
+        <p className="hero__subheading">Cloneables FOR WEBFLOW</p>
+        <h1 className="hero__heading">
+          Frequently Asked Questions Accordions Webflow Cloneable Template
+        </h1>
+        <p className="hero__desc">
+          Want to receive a monthly email in your inbox with awesome free
+          Webflow cloneables, resources and more? Please submit your email
+          below.
+        </p>
+        <div className="hero-details">
+        <img className="hero-details__icon" src={iconCheck} alt="check-icon" />
+        <p className="hero-details__desc">
+          It&apos;s <strong className="hero-details__percentage">100%</strong> free and we will never send more than
+          one email per month
+        </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
