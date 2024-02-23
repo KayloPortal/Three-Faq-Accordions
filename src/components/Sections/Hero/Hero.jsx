45360f58851dc098438e2c746479c3c3ef8dd306
kayloportal <pinobaxp@gmail.com>
2024-02-23 16:59:27 +0330

    added/ Hero styles

--- src/components/Sections/Hero/Hero.jsx
+++ src/components/Sections/Hero/Hero.jsx
@@ -6,11 +6,11 @@ function Hero() {
   return (
     <div className="hero">
       <div className="hero-container | container">
-        <p className="hero__subheading">Cloneables FOR WEBFLOW</p>
-        <h1 className="hero__heading">
+        <p className="hero__subheading | clr-primary-100 fw-medium fs-other-400">Cloneables FOR WEBFLOW</p>
+        <h1 className="hero__heading | clr-neutral-800 fw-bold fs-heading-100">
           Frequently Asked Questions Accordions Webflow Cloneable Template
         </h1>
-        <p className="hero__desc">
+        <p className="hero__desc | fs-paragraph-200">
           Want to receive a monthly email in your inbox with awesome free
           Webflow cloneables, resources and more? Please submit your email
           below.
@@ -19,7 +19,7 @@ function Hero() {
         <div className="hero-details">
         <img className="hero-details__icon" src={iconCheck} alt="check-icon" />
         <p className="hero-details__desc">
-          It&apos;s <strong className="hero-details__percentage">100%</strong> free and we will never send more than
+          It&apos;s <span className="hero-details__percentage | clr-primary-100">100%</span> free and we will never send more than
           one email per month
         </p>
         </div>
