96580ed4e1ac4e3a99503e67eaa40f6672a965a4
kayloportal <pinobaxp@gmail.com>
2024-02-23 16:30:42 +0330

    updated/ EmailInput styles

--- src/components/globals/EmailInput/EmailInput.jsx
+++ src/components/globals/EmailInput/EmailInput.jsx
@@ -8,14 +8,14 @@ function EmailInput() {
   return (
     <form className="email-form | fs-other-500" action="">
       <input
-        className="email-form__input | clr-neutral-600 fw-regular"
+        className="email-form__input | fw-regular"
         placeholder="Enter your email"
         value={email}
         onChange={({target}) => setemail(target.value)}
         name="email"
         type="text"
       />
-      <button className="emailt-form__submit | clr-neutral-100 fw-bold" type="submit">
+      <button className="email-form__submit | clr-neutral-100 fw-bold" type="submit">
         Subscribe
       </button>
     </form>
