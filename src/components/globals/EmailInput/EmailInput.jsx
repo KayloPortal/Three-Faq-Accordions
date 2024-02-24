dc4de5e4125f02b4d955f201b905d470e4900720
kayloportal <pinobaxp@gmail.com>
2024-02-24 19:13:15 +0330

    updated/ added validating feature to the InputForm

--- src/components/globals/EmailInput/EmailInput.jsx
+++ src/components/globals/EmailInput/EmailInput.jsx
@@ -1,23 +1,56 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./EmailInput.css";
+import toast, { Toaster } from "react-hot-toast";
 
 function EmailInput() {
-  const [email, setemail] = useState("")
+  const [email, setEmail] = useState("");
+  // When an API is available, this will asked from the API. Because I don't have an api here, I use this array just for testing and a showcase.
+  const emailsSubmitted = useRef([])
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  const submitData = (e) => {
+    e.preventDefault();
+    const match = email.match(emailRegex);
+    // match ? ss
+    if (match !== null && match[0] === email) {
+      emailsSubmitted.current.includes(email) ? renderError("This email is joined before") : sendData()
+
+    } else {
+      renderError("Please enter a valid email");
+    }
+  };
+
+  const sendData = () => {
+    emailsSubmitted.current.push(email)
+    toast.success("You joined the newslatter!");
+  };
+
+  const renderError = (message) => {
+    toast.error(message);
+  };
 
   return (
-    <form className="email-form | fs-other-500 fs-other-300-m" action="">
+    <form
+      className="email-form | fs-other-500 fs-other-300-m"
+      action=""
+      onSubmit={submitData}
+    >
       <input
         className="email-form__input | fw-regular"
         placeholder="Enter your email"
         value={email}
-        onChange={({target}) => setemail(target.value)}
+        onChange={({ target }) => setEmail(target.value)}
         name="email"
         type="text"
       />
-      <button className="email-form__submit | clr-neutral-100 fw-bold" type="submit">
+      <button
+        className="email-form__submit | clr-neutral-100 fw-bold"
+        type="submit"
+      >
         Subscribe
       </button>
+      <Toaster />
     </form>
   );
 }
