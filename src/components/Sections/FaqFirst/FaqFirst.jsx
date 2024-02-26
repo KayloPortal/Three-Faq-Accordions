cde332a8ea1d26936af9911bd2544e67ecbfc49a
kayloportal <pinobaxp@gmail.com>
2024-02-26 18:45:50 +0330

    updated/ FaqFirst.jsx, maked accordion content dynamic

--- src/components/Sections/FaqFirst/FaqFirst.jsx
+++ src/components/Sections/FaqFirst/FaqFirst.jsx
@@ -1,25 +1,43 @@
 import "./FaqFirst.css";
 import Faq from "/src/components/globals/Faq/Faq";
-import Accordion from "/src/components/globals/accordion/accordion";
+import Accordion from "/src/components/globals/Accordion/Accordion";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { nanoid } from "nanoid";
 
 function FaqFirst() {
+  const faqData = [
+    {
+      title: "What is Webflow and why is it the best website builder?",
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      title: "What is your favorite template from BRIX Templates?",
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      title: "How do you clone a Webflow Template from the Showcase?",
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      title: "Why is BRIX Templates the best Webflow agency out there?",
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+  ];
+
   return (
     <div className="faq">
       <Faq faqHeader="1. FAQ Accordion V1">
-        <Accordion>
-          <div>
-            <h3 className="">
-              What is Webflow and why is it the best website builder?
-            </h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-          <div></div>
-        </Accordion>
+        {faqData.map((data) => (
+          <Accordion key={nanoid()}>
+            <div className="accordion-headings">
+              <h3 className="accordion__heading">{data.title}</h3>
+              <button className="accordion-toggle" aria-label="toggle ">
+              <ChevronRightIcon className="accordion-toggle__icon | h-6 w-6 text-blue-500" />
+            </button>
+            </div>
+            <p className="accordion__desc">{data.desc}</p>
+          </Accordion>
+        ))}
       </Faq>
     </div>
   );
