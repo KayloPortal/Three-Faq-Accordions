de41ae192c58aab825518a9040cd6a5061375342
kayloportal <pinobaxp@gmail.com>
2024-03-09 21:16:37 +0330

    hotfix/ faqSecond faqData: added one new accordion info

--- src/components/Sections/FaqSecond/FaqSecond.jsx
+++ src/components/Sections/FaqSecond/FaqSecond.jsx
@@ -52,6 +52,10 @@ function FaqSecond() {
     }),
     [
       {
+        title: "How do you integrate Jetboost with Webflow?",
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+      },
+      {
         title: "What is Webflow and why is it the best website builder?",
         desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
       },
