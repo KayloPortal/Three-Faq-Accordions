34dd5a959bfb1f44e03e0a2853d006d717c1e4f2
kayloportal <pinobaxp@gmail.com>
2024-03-12 16:17:20 +0330

    Footer Built

--- src/components/Sections/Footer/Footer.jsx
+++ src/components/Sections/Footer/Footer.jsx
@@ -1,8 +1,21 @@
 import "./Footer.css"
+import brixLogo from "/logo.svg"
+import EmailInput from "../../globals/EmailInput/EmailInput"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="footer-container | container">
+      <div>
+        <img className="footer__logo" src={brixLogo} alt="Brix Agency" />
+        <p className="footer__copyright">Copyright © BRIX Templates | All Rights Reserved</p>
+      </div>
+      <div>
+        <p className="footer__desc">Subscribe to receive free Webflow cloneables every month.</p>
+        <EmailInput />
+      </div>
+      </div>
+    </div>
   )
 }
 
